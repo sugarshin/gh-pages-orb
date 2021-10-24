0f8cfad8246d14330a2a442608b88dd2a25d040d
Shingo Sato <shinsugar@gmail.com>
2021-10-24 15:35:23 +0900

    Upgrade for v1

---
+++ src/scripts/deploy.sh
@@ -0,0 +1,43 @@
+Setup() {
+    U=$(eval echo "$USERNAME")
+    R=$(eval echo "$REPONAME")
+    GIT_URL="git@${GIT_HOST}:${U}/${R}.git"
+    MESSAGE="Built artifacts of $(git rev-parse --short HEAD) [ci skip]"
+}
+
+SetCommitMessage() {
+    if [ -n "${COMMIT_MESSAGE}" ]; then
+        MESSAGE="${COMMIT_MESSAGE}"
+    fi
+}
+
+Clone() {
+    rm -rf "${TMP_DIR}"
+    git clone --depth=1 "${GIT_URL}" -b "${BRANCH}" "${TMP_DIR}" || (git init "${TMP_DIR}" && cd "${TMP_DIR}" && git remote add origin "${GIT_URL}" && git checkout -b "${BRANCH}" && cd - || return)
+}
+
+UpdateAssets() {
+    # shellcheck disable=SC2115
+    rm -rf "${TMP_DIR}"/*
+    cp -R "${TMP_DIR}"/../"${DIST_DIR}"/. "${TMP_DIR}"
+}
+
+Push() {
+    cd "${TMP_DIR}" || return
+    git add --all
+    git commit -m "${MESSAGE}" || true
+    git push origin "${BRANCH}"
+}
+
+Main() {
+    Setup
+    SetCommitMessage
+    Clone
+    UpdateAssets
+    Push
+}
+
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    Main
+fi
